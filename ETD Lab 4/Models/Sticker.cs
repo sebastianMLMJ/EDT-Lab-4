ebc68bffb603519e64482cdf74950f04adcd2dcd
Tico-1998 <46880040+Tico-1998@users.noreply.github.com>
2019-04-10 21:01:01 -0600

    Diccionarios, falta vista

---
+++ ETD Lab 4/Models/Sticker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ETD_Lab_4.Models
+{
+    public class Sticker
+    {
+        public string Tipo;
+        public string Equipo;
+        public string Nombre;
+        public bool Obtenida;
+        public string Tipo_Especial;
+    }
+}
